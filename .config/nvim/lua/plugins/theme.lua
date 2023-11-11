37b066cbc7c3d4ce3313ccb23c08aa8570c7f733
zyadelhady <zyade40@gmail.com>
2023-11-11 23:35:15 +0200

    new theme

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,10 +1,19 @@
+-- return {
+--     'Mofiqul/vscode.nvim',
+--     lazy = false,
+--     priority = 999,
+--  config = function()
+--     vim.o.background = "dark"
+--     require("vscode").setup({})
+--     require("vscode").load()
+--   end,
+-- }
+--
 return {
-    'Mofiqul/vscode.nvim',
-    lazy = false,
-    priority = 999,
- config = function()
-    vim.o.background = "dark"
-    require("vscode").setup({})
-    require("vscode").load()
-  end,
+	"EdenEast/nightfox.nvim",
+	lazy = false,
+	pioriy = 1000,
+	config = function()
+		vim.cmd("colorscheme carbonfox")
+	end,
 }
