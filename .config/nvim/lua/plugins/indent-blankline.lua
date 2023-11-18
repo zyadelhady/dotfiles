cf5d4b26a2ac5c40dcbdcbad7be54800ad396483
zyadelhady <zyade40@gmail.com>
2023-11-19 01:05:41 +0200

    changed indentation style , added fold

--- .config/nvim/lua/plugins/indent-blankline.lua
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -1,22 +1,23 @@
 local highlight = {
-    "CursorColumn",
-    "Whitespace",
+	"CursorColumn",
+	"Whitespace",
 }
 
-local config = function ()
-  require("ibl").setup {
-    indent = { highlight = highlight, char = "" },
-    whitespace = {
-        highlight = highlight,
-        remove_blankline_trail = false,
-    },
-    scope = { enabled = true },
-}
+local config = function()
+	require("ibl").setup({
+		scope = {
+			enabled = true,
+			show_start = true,
+			show_end = true,
+			priority = 500,
+			highlight = { "Function", "Label" },
+		},
+	})
 end
 
-return { 
-  "lukas-reineke/indent-blankline.nvim",
-  lazy = false,
-  main = "ibl",
-  config= config
+return {
+	"lukas-reineke/indent-blankline.nvim",
+	lazy = false,
+	main = "ibl",
+	config = config,
 }
--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -185,9 +185,10 @@ local config = function()
 		enable_ms_build_load_projects_on_demand = false,
 		enable_roslyn_analyzers = false,
 		organize_imports_on_format = true,
-		enable_import_completion = false,
+		enable_import_completion = true,
 		sdk_include_prereleases = true,
 		analyze_open_documents_only = false,
+		show_completion_items_from_unimported_namespaces = true,
 	})
 
 	-- --- diagnostics
