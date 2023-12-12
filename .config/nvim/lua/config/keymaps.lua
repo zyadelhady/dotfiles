0d445163c42b3bd633aa563c85d8fb7bb6c7419b
zyadelhady <zyade40@gmail.com>
2023-12-12 10:17:46 +0200

    work environment

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -11,9 +11,9 @@ keymap("n", "<C-l>", "<C-w>l", opts) -- Navigate Right
 -- Window Management
 
 keymap("n", "<leader>sr", ":vsplit<CR>", opts) -- Split Vertically
-keymap("n", "<leader>sd", ":split<CR>", opts) -- Split Horizontally
-keymap("n", "<leader>wc", "<C-w>q", opts) -- close pane
-keymap("n", "<leader>ww", "<C-w>w", opts) -- switch panes
+keymap("n", "<leader>sd", ":split<CR>", opts)  -- Split Horizontally
+keymap("n", "<leader>wc", "<C-w>q", opts)      -- close pane
+keymap("n", "<leader>ww", "<C-w>w", opts)      -- switch panes
 
 -- Identing
 keymap("v", "<S-TAB>", "<gv")
@@ -25,3 +25,21 @@ vim.api.nvim_set_keymap("v", "<C-_>", "gcc", { noremap = false })
 
 vim.api.nvim_set_keymap("n", "<A-s>", ":LspOverloadsSignature<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("i", "<A-s>", "<cmd>LspOverloadsSignature<CR>", { noremap = true, silent = true })
+
+keymap("n", "<leader>tr", "<Cmd>OverseerRun<CR>", opts)
+keymap("n", "<leader>to", "<Cmd>OverseerToggle<CR>", opts)
+
+keymap("n", "<F5>", "<Cmd>lua require'dap'.continue()<CR>", opts)
+keymap("n", "<F10>", "<Cmd>lua require'dap'.step_over()<CR>", opts)
+keymap("n", "<F11>", "<Cmd>lua require'dap'.step_into()<CR>", opts)
+keymap("n", "<F12>", "<Cmd>lua require'dap'.step_out()<CR>", opts)
+keymap("n", "<leader>b", "<Cmd>lua require'dap'.toggle_breakpoint()<CR>", opts)
+keymap("n", "<leader>B", "<Cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>", opts)
+keymap(
+  "n",
+  "<leader>lp",
+  "<Cmd>lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>",
+  opts
+)
+keymap("n", "<leader>dr", "<Cmd>lua require'dap'.repl.open()<CR>", opts)
+keymap("n", "<leader>dl", "<Cmd>lua require'dap'.run_last()<CR>", opts)
