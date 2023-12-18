36e287f69a412e5726d4708e042796d9f77de55b
zyadelhady <zyade40@gmail.com>
2023-12-18 13:40:55 +0200

    added sessions

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -45,21 +45,3 @@ return {
 		vim.cmd("colorscheme kanagawa-dragon")
 	end,
 }
--- return {
--- 	"EdenEast/nightfox.nvim",
--- 	lazy = false,
--- 	pioriy = 1000,
--- 	config = function()
--- 		vim.cmd("colorscheme carbonfox")
--- 	end,
--- }
-
--- return {
--- 	"tinted-theming/base16-vim",
--- 	lazy = false,
--- 	piority = 1000,
--- 	config = function()
--- 		vim.g.base16_colorspace = 256
--- 		vim.cmd("colorscheme base16-default-dark")
--- 	end,
--- }
