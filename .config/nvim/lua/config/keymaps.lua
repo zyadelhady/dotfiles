25e807c4c6f6209f83f1db76801c74cfff166f23
zyadelhady <zyade40@gmail.com>
2023-12-19 13:51:09 +0200

    added keymaps to debugger

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -11,9 +11,9 @@ keymap("n", "<C-l>", "<C-w>l", opts) -- Navigate Right
 -- Window Management
 
 keymap("n", "<leader>sr", ":vsplit<CR>", opts) -- Split Vertically
-keymap("n", "<leader>sd", ":split<CR>", opts)  -- Split Horizontally
-keymap("n", "<leader>wc", "<C-w>q", opts)      -- close pane
-keymap("n", "<leader>ww", "<C-w>w", opts)      -- switch panes
+keymap("n", "<leader>sd", ":split<CR>", opts) -- Split Horizontally
+keymap("n", "<leader>wc", "<C-w>q", opts) -- close pane
+keymap("n", "<leader>ww", "<C-w>w", opts) -- switch panes
 
 -- Increase and Decrease Height
 keymap("n", "<leader>hi", ":resize +2<CR>", opts)
@@ -27,16 +27,32 @@ keymap("n", "<leader>wd", ":vertical resize -2<CR>", opts)
 keymap("v", "<S-TAB>", "<gv")
 keymap("v", "<TAB>", ">gv")
 
+--copy all buffer
+keymap({ "n", "v" }, "<leader>ya", "<CMD>%y<CR>", opts)
+
 -- Comments
-vim.api.nvim_set_keymap("n", "<C-_>", "gcc", { noremap = false })
-vim.api.nvim_set_keymap("v", "<C-_>", "gcc", { noremap = false })
+vim.api.nvim_set_keymap("n", "<leader>c", "gcc", { noremap = false })
+vim.api.nvim_set_keymap("v", "<leader>c", "gcc<ESC>", { noremap = false })
 
 vim.api.nvim_set_keymap("n", "<A-s>", ":LspOverloadsSignature<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("i", "<A-s>", "<cmd>LspOverloadsSignature<CR>", { noremap = true, silent = true })
 
 keymap("n", "<leader>tr", "<Cmd>OverseerRun<CR>", opts)
 keymap("n", "<leader>to", "<Cmd>OverseerToggle<CR>", opts)
-
+vim.keymap.set({ "n", "v" }, "<Leader>dh", function()
+	require("dap.ui.widgets").hover()
+end)
+vim.keymap.set({ "n", "v" }, "<Leader>dp", function()
+	require("dap.ui.widgets").preview()
+end)
+vim.keymap.set("n", "<Leader>df", function()
+	local widgets = require("dap.ui.widgets")
+	widgets.centered_float(widgets.frames)
+end)
+vim.keymap.set("n", "<Leader>ds", function()
+	local widgets = require("dap.ui.widgets")
+	widgets.centered_float(widgets.scopes)
+end)
 keymap("n", "<F5>", "<Cmd>lua require'dap'.continue()<CR>", opts)
 keymap("n", "<F10>", "<Cmd>lua require'dap'.step_over()<CR>", opts)
 keymap("n", "<F11>", "<Cmd>lua require'dap'.step_into()<CR>", opts)
@@ -44,10 +60,10 @@ keymap("n", "<F12>", "<Cmd>lua require'dap'.step_out()<CR>", opts)
 keymap("n", "<leader>b", "<Cmd>lua require'dap'.toggle_breakpoint()<CR>", opts)
 keymap("n", "<leader>B", "<Cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>", opts)
 keymap(
-  "n",
-  "<leader>lp",
-  "<Cmd>lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>",
-  opts
+	"n",
+	"<leader>lp",
+	"<Cmd>lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>",
+	opts
 )
 keymap("n", "<leader>dr", "<Cmd>lua require'dap'.repl.open()<CR>", opts)
 keymap("n", "<leader>dl", "<Cmd>lua require'dap'.run_last()<CR>", opts)
